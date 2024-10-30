634e4a186320effb21f25b992ed44d44d6c417f9
kotkala <91318800+kotkala@users.noreply.github.com>
2024-10-30 10:08:41 +0700

    commit map

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,12 +1,14 @@
+// src/navigators/TabNavigator.tsx
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {COLORS} from '../theme/theme';
-import {BlurView} from '@react-native-community/blur';
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { COLORS } from '../theme/theme';
+import { BlurView } from '@react-native-community/blur';
 import HomeScreen from '../screens/HomeScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import CartScreen from '../screens/CartScreen';
 import OrderHistoryScreen from '../screens/OrderHistoryScreen';
+import MapScreen from '../screens/MapScreen'; // Import MapScreen
 import CustomIcon from '../components/CustomIcon';
 
 const Tab = createBottomTabNavigator();
@@ -31,7 +33,7 @@ const TabNavigator = () => {
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: ({focused, color, size}) => (
+          tabBarIcon: ({ focused, color, size }) => (
             <CustomIcon
               name="home"
               size={25}
@@ -40,12 +42,28 @@ const TabNavigator = () => {
               }
             />
           ),
-        }}></Tab.Screen>
+        }}
+      />
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <CustomIcon
+              name="home" // Sử dụng icon giống Home
+              size={25}
+              color={
+                focused ? COLORS.primaryOrangeHex : COLORS.primaryLightGreyHex
+              }
+            />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Cart"
         component={CartScreen}
         options={{
-          tabBarIcon: ({focused, color, size}) => (
+          tabBarIcon: ({ focused, color, size }) => (
             <CustomIcon
               name="cart"
               size={25}
@@ -54,12 +72,13 @@ const TabNavigator = () => {
               }
             />
           ),
-        }}></Tab.Screen>
+        }}
+      />
       <Tab.Screen
         name="Favorite"
         component={FavoritesScreen}
         options={{
-          tabBarIcon: ({focused, color, size}) => (
+          tabBarIcon: ({ focused, color, size }) => (
             <CustomIcon
               name="like"
               size={25}
@@ -68,12 +87,13 @@ const TabNavigator = () => {
               }
             />
           ),
-        }}></Tab.Screen>
+        }}
+      />
       <Tab.Screen
         name="History"
         component={OrderHistoryScreen}
         options={{
-          tabBarIcon: ({focused, color, size}) => (
+          tabBarIcon: ({ focused, color, size }) => (
             <CustomIcon
               name="bell"
               size={25}
@@ -82,7 +102,8 @@ const TabNavigator = () => {
               }
             />
           ),
-        }}></Tab.Screen>
+        }}
+      />
     </Tab.Navigator>
   );
 };
